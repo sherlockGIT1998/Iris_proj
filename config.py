f9a5d27e4f31fa40b7b5a92f12a64f58a667ce3b
carbha01234 <charderajnee20@gmail.com>
2024-09-23 00:43:08 +0530

    All files are added...

---
+++ config.py
@@ -0,0 +1,3 @@
+MODEL_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Classification\IRIS_Dataset__\Project_app\IRIS.pkl'
+
+JSON_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Classification\IRIS_Dataset__\Project_app\IRIS.json'
